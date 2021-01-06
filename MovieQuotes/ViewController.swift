c84b93a998857ed80168a39c0c07dcfbe9ab6298
sunh5 <sunh5@rose-hulman.edu>
2021-01-05 20:53:09 -0500

    Initial Commit

---
+++ MovieQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuotes
+//
+//  Created by Haoxuan Sun on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
