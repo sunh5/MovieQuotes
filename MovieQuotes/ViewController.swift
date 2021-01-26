af5b409edf23472660bc2e31fd39344137c34275
sunh5 <sunh5@rose-hulman.edu>
2021-01-26 00:00:43 -0500

    finish DIY suitable view via a UIcontroller

--- MovieQuotes/ViewController.swift
+++ MovieQuotes/ViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let TempCell = "TempCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TempCell, for: indexPath)
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
