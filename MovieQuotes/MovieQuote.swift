96410bd3f339ec54b102f4ab7619619acff2959d
sunh5 <sunh5@rose-hulman.edu>
2021-01-26 00:36:48 -0500

    table ready to go

---
+++ MovieQuotes/MovieQuote.swift
@@ -0,0 +1,18 @@
+//
+//  MovieQuote.swift
+//  MovieQuotes
+//
+//  Created by Haoxuan Sun on 1/26/21.
+//
+
+import Foundation
+
+class MovieQuote{
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String){
+        self.quote = quote
+        self.movie = movie
+    }
+}
--- MovieQuotes/MovieQuotesTabTableViewController.swift
+++ MovieQuotes/MovieQuotesTabTableViewController.swift
@@ -9,18 +9,32 @@ import UIKit
 
 class MovieQuotesTabTableViewController: UITableViewController {
     var movieQuoteCellIdentifier = "MovieQuoteCell"
-    var names = ["Ray", "911","Dave", "shuai", "Ji"]
+//    var names = ["Ray", "911","Dave", "shuai", "Ji"]
+    var movieQuotes = [MovieQuote]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddQuoteDialog))
+        
+        movieQuotes.append(MovieQuote(quote: "I be back", movie: "The ternimater"))
+        movieQuotes.append(MovieQuote(quote: "Yo Ray", movie: "ROcky"))
+    }
+    
+    @objc func showAddQuoteDialog(){
+        print("you press add button")
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection sectioin: Int) -> Int {
         //super.viewDidLoad()
         // Do any additional setup after loading the view.
-        return names.count
+        return movieQuotes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: movieQuoteCellIdentifier, for: indexPath)
+        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+        cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
         
-        cell.textLabel?.text = names[indexPath.row]
         return cell
     }
     
